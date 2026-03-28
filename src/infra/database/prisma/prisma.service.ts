94f63eb3793c6fb60f6f88151a173beced290a1b
José Victor <josevictornascimento2016@gmail.com>
2026-03-28 14:39:18 -0300

    feat: :sparkles: implement PrismaService for database connection management

---
+++ src/infra/database/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "generated/prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = `${process.env.DATABASE_URL}`;
+    const adapter = new PrismaPg({ connectionString });
+
+    super({
+      log: ["error", "warn"],
+      adapter,
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
