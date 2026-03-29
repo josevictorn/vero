bd6cf216a00bcad556d2bd1c129555cdff95e339
José Victor <josevictornascimento2016@gmail.com>
2026-03-29 15:00:48 -0300

    feat: :sparkles: add InMemoryAccountsRepository implementation for testing purposes

---
+++ test/repositories/in-memory-accounts-repository.ts
@@ -0,0 +1,23 @@
+import type { AccountsRepository } from "@/domain/iam/application/repositories/accounts-repository";
+import type { Account } from "@/domain/iam/enterprise/entities/account";
+import type { Admin } from "@/domain/iam/enterprise/entities/admin.ts";
+
+export class InMemoryAccountsRepository implements AccountsRepository {
+  // biome-ignore lint/style/useConsistentMemberAccessibility: This is a test class, so we can keep the items array public for easier access in tests.
+  public items: Account[] = [];
+
+  findByEmail(email: string) {
+    const admin = this.items.find((item) => item.email === email);
+
+    if (!admin) {
+      return Promise.resolve(null);
+    }
+
+    return Promise.resolve(admin);
+  }
+
+  create(admin: Admin) {
+    this.items.push(admin);
+    return Promise.resolve();
+  }
+}
