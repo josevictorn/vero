982f76c6ce8f6a6b0e4464a9abe3ccd876b7ac7f
José Victor <josevictornascimento2016@gmail.com>
2026-03-28 22:30:23 -0300

    feat: :sparkles: add AdminFactory for creating PrismaAdmin instances

--- test/factories/make-admin.ts
+++ test/factories/make-admin.ts
@@ -1,6 +1,9 @@
 import { faker } from "@faker-js/faker/locale/pt_BR";
+import { Inject, Injectable } from "@nestjs/common";
 import type { UniqueEntityID } from "@/core/entity/unique-entity-id";
 import { Admin, type AdminProps } from "@/domain/iam/enterprise/entities/admin";
+import { PrismaAdminMapper } from "@/infra/database/prisma/mappers/prisma-admin-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 
 export function makeAdmin(
   override: Partial<AdminProps> = {},
@@ -17,4 +20,22 @@ export function makeAdmin(
   );
 
   return admin;
+}
+
+@Injectable()
+export class AdminFactory {
+  constructor(
+    @Inject(PrismaService)
+    private readonly prisma: PrismaService
+  ) {}
+
+  async makePrismaAdmin(data: Partial<AdminProps> = {}) {
+    const admin = makeAdmin(data);
+
+    await this.prisma.user.create({
+      data: PrismaAdminMapper.toPrisma(admin),
+    });
+
+    return admin;
+  }
 }
