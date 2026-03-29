17a2858399227d8e985e67ab72106f83583affbf
José Victor <josevictornascimento2016@gmail.com>
2026-03-28 22:30:04 -0300

    feat: :sparkles: implement JwtEncrypter class for JWT encryption and update CryptographyModule to include it

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,10 +1,16 @@
 import { Module } from "@nestjs/common";
+import { Encrypter } from "@/domain/iam/application/cryptography/encrypter";
 import { HashComparer } from "@/domain/iam/application/cryptography/hash-comparer.ts";
 import { HashGenerator } from "@/domain/iam/application/cryptography/hash-generator.ts";
 import { ArgonHasher } from "@/infra/cryptography/argon-hasher.ts";
+import { JwtEncrypter } from "./jwt-encrypter";
 
 @Module({
   providers: [
+    {
+      provide: Encrypter,
+      useClass: JwtEncrypter,
+    },
     {
       provide: HashComparer,
       useClass: ArgonHasher,
@@ -14,6 +20,6 @@ import { ArgonHasher } from "@/infra/cryptography/argon-hasher.ts";
       useClass: ArgonHasher,
     },
   ],
-  exports: [HashComparer, HashGenerator],
+  exports: [HashComparer, HashGenerator, Encrypter],
 })
 export class CryptographyModule {}
