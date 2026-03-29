b6ce254e085b082a47bf0615bdada4aeec1cc1c6
José Victor <josevictornascimento2016@gmail.com>
2026-03-29 15:00:06 -0300

    feat: :sparkles: implement Account entity and refactor Admin to extend Account

---
+++ src/domain/iam/enterprise/entities/account.ts
@@ -0,0 +1,38 @@
+import type { UserRole } from "@prisma/client";
+import { Entity } from "@/core/entity/entity.ts";
+import type { UniqueEntityID } from "@/core/entity/unique-entity-id.ts";
+
+export interface AccountProps {
+  email: string;
+  name: string;
+  password: string;
+  role: UserRole;
+}
+
+export class Account extends Entity<AccountProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get role() {
+    return this.props.role;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  set password(password: string) {
+    this.props.password = password;
+  }
+
+  static create(props: AccountProps, id?: UniqueEntityID) {
+    const account = new Account(props, id);
+
+    return account;
+  }
+}
--- src/domain/iam/enterprise/entities/admin.ts
+++ src/domain/iam/enterprise/entities/admin.ts
@@ -1,32 +1,5 @@
-import { Entity } from "@/core/entity/entity.ts";
-import type { UniqueEntityID } from "@/core/entity/unique-entity-id.ts";
+import { Account, type AccountProps } from "./account";
 
-export interface AdminProps {
-  email: string;
-  name: string;
-  password: string;
-}
+export interface AdminProps extends AccountProps {}
 
-export class Admin extends Entity<AdminProps> {
-  get name() {
-    return this.props.name;
-  }
-
-  get email() {
-    return this.props.email;
-  }
-
-  get password() {
-    return this.props.password;
-  }
-
-  set password(password: string) {
-    this.props.password = password;
-  }
-
-  static create(props: AdminProps, id?: UniqueEntityID) {
-    const admin = new Admin(props, id);
-
-    return admin;
-  }
-}
+export class Admin extends Account {}
