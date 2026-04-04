000b52448ff502f55bcec7a0bb3556dbdd627718
José Victor <josevictornascimento2016@gmail.com>
2026-04-04 10:13:29 -0300

    refactor: clean up imports and formatting in GetUserProfileController

--- src/infra/http/controllers/iam/get-user-profile.controller.ts
+++ src/infra/http/controllers/iam/get-user-profile.controller.ts
@@ -1,14 +1,22 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  Inject,
+  NotFoundException,
+  UseGuards,
+} from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AccountNotFoundExistsError } from "@/domain/iam/application/use-cases/errors/acount-not-found-error";
 import { GetUserProfileUseCase } from "@/domain/iam/application/use-cases/get-user-profile";
+import { UserRole } from "@/domain/iam/enterprise/entities/value-objects/user-role";
 import { Action } from "@/infra/auth/casl/actions";
 import { CheckPolicies } from "@/infra/auth/casl/check-policies.decorator";
 import { PoliciesGuard } from "@/infra/auth/casl/policies.guard";
 import { CurrentUser } from "@/infra/auth/current-user-decorator";
 import type { UserPayload } from "@/infra/auth/jwt.strategy";
-import { BadRequestException, Controller, Get, HttpCode, Inject, NotFoundException, Request, UseGuards } from "@nestjs/common";
-import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AccountPresenter } from "../../presenters/account-presenter";
-import { UserRole } from "@/domain/iam/enterprise/entities/value-objects/user-role";
 
 @ApiTags("Accounts")
 @Controller("/accounts/profile")
@@ -33,15 +41,15 @@ export class GetUserProfileController {
             id: { type: "string", format: "uuid" },
             name: { type: "string" },
             email: { type: "string", format: "email" },
-            role: { 
-              type: "string", 
+            role: {
+              type: "string",
               enum: [
                 UserRole.ADMIN,
                 UserRole.CLIENT,
                 UserRole.ASSISTANT,
                 UserRole.FINANCE,
                 UserRole.LAWYER,
-              ], 
+              ],
             },
           },
         },
@@ -56,15 +64,16 @@ export class GetUserProfileController {
       properties: {
         message: { type: "string", example: "Account with ID 123 not found" },
       },
-    }
+    },
   })
   @ApiResponse({
     status: 500,
     description: "Internal server error",
   })
-
-  async handle(@CurrentUser() userPayload: UserPayload,) {    
-    const result = await this.getUserProfile.execute({userId: userPayload.sub});
+  async handle(@CurrentUser() userPayload: UserPayload) {
+    const result = await this.getUserProfile.execute({
+      userId: userPayload.sub,
+    });
 
     if (result.isLeft()) {
       const error = result.value;
