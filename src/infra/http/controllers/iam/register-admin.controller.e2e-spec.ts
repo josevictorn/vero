2d5abe122d1e583aee024572daf309620fc647f6
José Victor <josevictornascimento2016@gmail.com>
2026-04-05 11:12:15 -0300

    fix: :test_tube: fix register admin e2e test

--- src/infra/http/controllers/iam/register-admin.controller.e2e-spec.ts
+++ src/infra/http/controllers/iam/register-admin.controller.e2e-spec.ts
@@ -1,28 +1,46 @@
 import type { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
+import { AccountFactory } from "test/factories/make-account";
+import { UserRole } from "@/domain/iam/enterprise/entities/value-objects/user-role";
 import { AppModule } from "@/infra/app.module.ts";
+import { DatabaseModule } from "@/infra/database/database.module";
 import { PrismaService } from "@/infra/database/prisma/prisma.service.ts";
 
 describe("Register Admin Controller (e2e)", () => {
   let app: INestApplication;
+  let accountFactory: AccountFactory;
   let prisma: PrismaService;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [AccountFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
+    accountFactory = moduleRef.get(AccountFactory);
     prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
   test("[POST] /accounts/admin", async () => {
+    const adminAccount = await accountFactory.makePrismaAccount({
+      role: UserRole.ADMIN,
+    });
+
+    const accessToken = jwt.sign({
+      sub: adminAccount.id.toString(),
+    });
+
     const response = await request(app.getHttpServer())
       .post("/accounts/admin")
+      .set("Authorization", `Bearer ${accessToken}`)
       .send({
         name: "John Doe",
         email: "john.doe@example.com",
