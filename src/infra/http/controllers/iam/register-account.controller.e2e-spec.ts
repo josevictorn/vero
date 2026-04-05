ac2d1917948f55a708afff0d24b25ce52277bd32
José Victor <josevictornascimento2016@gmail.com>
2026-04-05 11:12:38 -0300

    fix: :test_tube: fix register account e2e test

--- src/infra/http/controllers/iam/register-account.controller.e2e-spec.ts
+++ src/infra/http/controllers/iam/register-account.controller.e2e-spec.ts
@@ -1,32 +1,51 @@
 import type { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
-import { UserRole } from "generated/prisma/client";
 import request from "supertest";
+import { AccountFactory } from "test/factories/make-account";
+import { UserRole } from "@/domain/iam/enterprise/entities/value-objects/user-role";
 import { AppModule } from "@/infra/app.module.ts";
+import { DatabaseModule } from "@/infra/database/database.module";
 import { PrismaService } from "@/infra/database/prisma/prisma.service.ts";
 
 describe("Register Admin Controller (e2e)", () => {
   let app: INestApplication;
+  let accountFactory: AccountFactory;
   let prisma: PrismaService;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [AccountFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
+    accountFactory = moduleRef.get(AccountFactory);
     prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
   test("[POST] /accounts", async () => {
-    const response = await request(app.getHttpServer()).post("/accounts").send({
-      name: "John Doe",
-      email: "john.doe@example.com",
-      role: UserRole.CLIENT,
+    const adminAccount = await accountFactory.makePrismaAccount({
+      role: UserRole.ADMIN,
     });
+
+    const accessToken = jwt.sign({
+      sub: adminAccount.id.toString(),
+    });
+
+    const response = await request(app.getHttpServer())
+      .post("/accounts")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        name: "John Doe",
+        email: "john.doe@example.com",
+        role: UserRole.CLIENT,
+      });
 
     expect(response.status).toBe(201);
 
