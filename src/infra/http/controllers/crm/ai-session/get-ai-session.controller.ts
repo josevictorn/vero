f479d9c8aa149ab5f37c25cb7513924efe59e636
André Lira <andrelira50@gmail.com>
2026-04-08 23:38:47 -0300

    docs: better exemplification on swagger

--- src/infra/http/controllers/crm/ai-session/get-ai-session.controller.ts
+++ src/infra/http/controllers/crm/ai-session/get-ai-session.controller.ts
@@ -24,8 +24,8 @@ export class GetAISessionController {
     @HttpCode(200)
     @ApiParam({
         name: "id",
-        type: "uuid",
-        example: "{id}",
+        type: "string",
+        example: "123e4567-e89b-12d3-a456-426614174000",
     })
     @ApiResponse({
         status: 200,
