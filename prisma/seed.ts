bc097c2654ae266a5a2520476c237c62210c5b0e
André Lira <andrelira50@gmail.com>
2026-04-08 22:39:06 -0300

    fix: fixing seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,17 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "generated/prisma/client";
 
-const prisma = new PrismaClient();
 
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+});
+//deixei hardocodado pelo exemplo
 async function main() {
-  const defaultWorkspaceId = "default-workspace";
+  const defaultWorkspaceId = "4bd6c784-33a8-4af9-b3f2-ac2710475cf8";
 
   const workspace = await prisma.workspace.upsert({
     where: { id: defaultWorkspaceId },
-    update: {}, // Não faz nada se já existir
+    update: {},
     create: {
       id: defaultWorkspaceId,
       name: "Escritório Modelo",
@@ -17,15 +21,14 @@ async function main() {
     },
   });
 
-  console.log({ workspace });
+  console.log('Seed executado com sucesso:', { workspace });
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
+  .catch((e) => {
     console.error(e);
-    await prisma.$disconnect();
     process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
   });
